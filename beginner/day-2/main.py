52f8bff14dc446f331de3392735d2086d6fdee91
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2022-12-02 03:05:05 +0200

    Create main.py

---
+++ beginner/day-2/main.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+left = 90 - int(age)
+left1 = 52 * left
+left2 = 365 * left
+print(f"You have {left2} days,{left1} weeks, {left} years")
+
+
